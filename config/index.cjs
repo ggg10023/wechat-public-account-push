cb813de4192494510179a209f8e63c8837719abc
ggg10023 <116228929+ggg10023@users.noreply.github.com>
2022-10-20 07:21:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCvXp5-Pt0C5qQh8ms2Eg5U8Q2nA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'VB2H9uQElaA5bzvswAFT2HsjOeDuD29WE3l-PImfqrs',
+      useTemplateId: '7OD4c9iUnGnN_GMIH96hFobz3ovWV2LGe1_vqW2y7fU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-27',
       festivals: [
@@ -46,7 +46,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'VB2H9uQElaA5bzvswAFT2HsjOeDuD29WE3l-PImfqrs',
+  CALLBACK_TEMPLATE_ID: '7OD4c9iUnGnN_GMIH96hFobz3ovWV2LGe1_vqW2y7fU',
 
   CALLBACK_USERS: [
     {
